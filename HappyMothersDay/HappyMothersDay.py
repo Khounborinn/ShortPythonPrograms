e6268f2ec094fe576f76122ec4944f845ae26c03
Nolan Khounborin <khounborinn@gmail.com>
2020-05-10 12:51:12 -0400

    Added HMD program

---
+++ HappyMothersDay/HappyMothersDay.py
@@ -0,0 +1,23 @@
+import time
+import random
+import colorama
+from colorama import init
+
+init(convert=True)
+colors = list(vars(colorama.Fore).values())
+
+while True:
+    time.sleep(0.5)
+    print(
+        " " * random.randint(0, 150)
+        + f"{colorama.Style.BRIGHT} {random.choice(colors)}Happy {random.choice(colors)}Mother's {random.choice(colors)}Day!",
+        end="",
+    )
+    print(
+        " " * random.randint(0,150)
+        + f"{colorama.Style.BRIGHT} {random.choice(colors)}Best {random.choice(colors)}Mom {random.choice(colors)}Ever!",
+        end="",
+    )
+
+    print(" " * random.randint(0, 150) + f"{colorama.Style.BRIGHT}*", end="")
+    print(" " * random.randint(0, 150) + f"{colorama.Style.BRIGHT}*")
---
+++ HappyMothersDay/README.md
@@ -0,0 +1,4 @@
+# HappyMothersDay
+This Program displays a message to your mother through the terminal.
+
+I used Spyder IDE and Anaconda Terminal.
